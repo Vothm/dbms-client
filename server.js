3bcd506c9254d668f5b41a6ac795846c196cc5af
Vonce <vincerosane@gmail.com>
2022-05-30 16:40:11 -0700

    death

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
